3f8ebf791e19aac63134a5d1f73593da2f958c0e
Gurpreet <gsaluja@ucsd.edu>
2024-02-02 23:33:46 -0800

    Add Lab5 files

---
+++ Lab5/todomvc/src/components/Graph.svelte
@@ -0,0 +1,42 @@
+<script>
+	// our interactive data
+	export let todo_category = [];
+
+	$: {
+		// interactive data here
+		console.log(JSON.stringify(todo_category));
+	}
+
+</script>
+
+<div class="visualization">
+</div>
+
+<style>
+	.visualization {
+		font: 25px sans-serif;
+		margin: auto;
+		margin-top: 1px;
+		text-align: middle;
+	}
+
+	/* dynamic classes for the tooltip */
+	.tooltip-hidden {
+		visibility: hidden;
+		font-family: "Nunito", sans-serif;
+		width: 200px;
+		position: absolute;
+	}
+
+	.tooltip-visible {
+		font: 25px sans-serif;
+		font-family: "Nunito", sans-serif;
+		visibility: visible;
+		background-color: #f0dba8;
+		border-radius: 10px;
+		width: 200px;
+		color: black;
+		position: absolute;
+		padding: 10px;
+	}
+</style>
